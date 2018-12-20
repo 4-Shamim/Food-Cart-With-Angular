255c72221f690a4153ce193e33c45d1af71009f7
Shamim <innocentb758@gmail.com>
2018-12-21 01:31:14 +0600

    Food Cart

---
+++ src/app/model/item.ts
@@ -0,0 +1,10 @@
+export class Item {
+
+    public itemName: string;
+    public itemType: string;
+    public restaurantName: string;
+    public location: string;
+    public price: number;
+    public vote: number;
+
+}
